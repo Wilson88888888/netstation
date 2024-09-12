cd93d5705992a4f9b9ac229e0739ad284b32783e
zhaokaien <zhaokaien@zhaokaiendeMacBook-Pro.local>
2024-09-12 13:39:53 +0800

    after delete commit

--- astro.config.ts
+++ astro.config.ts
@@ -28,7 +28,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  //base: '/netstation-pulish/',
+  base: '/netstation/',
   output: 'static',
 
   integrations: [
--- src/components/widgets/Paypal.jsx
+++ src/components/widgets/Paypal.jsx
@@ -8,7 +8,10 @@ const PaypalButton = () => {
         const script = document.createElement('script');
         script.id = 'paypal-sdk';
         //script.src = `https://www.paypal.com/sdk/js?client-id=AW8w-HMsHWRuOsB5I3uJSnc2ttC3b_W9yn5XHbOzXaa8_GnoEa0CP31yS0C-eU7g-7Es0l8Bp_OanXwZ`; //sandbox
-        script.src = `https://www.paypal.com/sdk/js?client-id=AZ34Ql31hxHWuvwOMq6QjJeRXSbe6yoOTwYF82acV6CcZPzj76-C-5MPMZNEwZZ-Yt-id1WlzASDIBnW`; //live
+        //script.src = `https://www.paypal.com/sdk/js?client-id=AZ34Ql31hxHWuvwOMq6QjJeRXSbe6yoOTwYF82acV6CcZPzj76-C-5MPMZNEwZZ-Yt-id1WlzASDIBnW`; //live
+        script.src = `https://www.paypal.com/sdk/js?client-id=AZ34Ql31hxHWuvwOMq6QjJeRXSbe6yoOTwYF82acV6CcZPzj76-C-5MPMZNEwZZ-Yt-id1WlzASDIBnW&currency=USD&intent=capture&disable-funding=card&components=buttons`;
+
+        
         script.async = true;
         script.onload = () => {
           window.paypal.Buttons({
