a18031534ec01e2b8f7343175b314dbb09cb591e
zhaokaien <zhaokaien@zhaokaiendeMacBook-Pro.local>
2024-09-13 09:45:28 +0800

    20240913-3

--- astro.config.ts
+++ astro.config.ts
@@ -28,9 +28,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
   hasExternalScripts ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
 
 export default defineConfig({
-  site: 'https://github.com/Wilson88888888/',
-  base: '/netstation/blob/main/docs/',
   outDir: 'docs',
+  base: '/netstation/',
   output: 'static',
 
   integrations: [
